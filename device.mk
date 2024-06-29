84a824108857752f857bd13bacdb30e751111a58
Giovanni Ricca <giovanniricca@proton.me>
2024-06-29 23:32:41 +0530

    [PATCH] begonia: Patch libsource to load the old graphicsbuffer symbol
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- device.mk
+++ device.mk
@@ -290,6 +290,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+PRODUCT_PACKAGES += \
+    libui_shim.vendor
+
 # MTK In-Call volumes adjusting service
 PRODUCT_PACKAGES += \
     MtkInCallService
