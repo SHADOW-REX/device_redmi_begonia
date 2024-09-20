f5de99b386f8166d42082ed79876f837200f5012
alextrack2013 <alextrack2013@gmail.com>
2024-09-21 00:19:26 +0530

    begonia: Link Audio HAL against stock
     - Fixes audio playback in some cases with USB headphones.
    
    Signed-off-by: alextrack2013 <alextrack2013@gmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,10 @@ function blob_fixup {
             ;;
         vendor/lib/hw/audio.primary.mt6785.so | vendor/lib64/hw/audio.primary.mt6785.so)
             "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
+	        ;;
+    	vendor/lib/hw/audio.usb.mt6785.so | vendor/lib64/hw/audio.usb.mt6785.so)
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
     	    ;;
         vendor/lib/libmtkcam_stdutils.so | vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
