b3f734dd45061015fdd2abb0de802c2ebcb6825f
Wahid Khan <wahidzk0091@gmail.com>
2024-09-25 13:31:20 +0530

    begonia: Add hardware/xiaomi to Soong namespaces
     - Following 24660f843193b3d1b53df8c80faf12966cac0a68, we now need hw/xiaomi as a dependency
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek \
+    hardware/xiaomi \
     hardware/mediatek/libmtkperf_client \
     hardware/lineage/interfaces/power-libperfmgr \
     $(LOCAL_PATH)
