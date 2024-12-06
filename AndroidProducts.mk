7337f4981398809628a54361f49ded0620f36ffd
SHADOW <33841315+SHADOW-REX@users.noreply.github.com>
2024-12-06 20:21:06 +0530

    begonia: Rising Bringup
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_begonia.mk
+    $(LOCAL_DIR)/lineage_begonia.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_begonia-user \
+    lineage_begonia-userdebug \
+    lineage_begonia-eng
--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -12,12 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from begonia device
-$(call inherit-product, device/redmi/begonia/device.mk)
+$(call inherit-product, device/xiaomi/begonia/device.mk)
 
-# Inherit some common Derpfest stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-TARGET_ENABLE_BLUR := true
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
@@ -36,15 +34,25 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
-#Derpfest Official
-DERP_BUILDTYPE := Official
+# disable/enable blur support
+TARGET_ENABLE_BLUR := true
+# Aperture Camera
+PRODUCT_NO_CAMERA := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := derp_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="begonia-user 11 RP1A.200720.011 V12.5.8.0.RGGMIXM release-keys" \
+    DeviceName="begonia" \
+    RISING_CHIPSET="MTK Helios G90T" \
+    RISING_MAINTAINER="SHADOW"
 
 BUILD_FINGERPRINT := "Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.8.0.RGGMIXM:user/release-keys"
 
