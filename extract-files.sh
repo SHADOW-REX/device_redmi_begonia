acb636e8706e30fe1115f4415cb4063eba6b0ecd
bengris32 <bengris32@protonmail.ch>
2025-02-11 20:59:51 +0530

    begonia: Drop MediaTek software OMX codecs * also partial revert https://github.com/Saikrishna1504/device_redmi_begonia/commit/97f4e1e46e047f7523ce16389334ccbb3e8ea55d
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -54,7 +54,7 @@ fi
 
 function blob_fixup {
     case "${1}" in
-        system_ext/lib64/libsource.so | vendor/lib/libMtkOmxVdecEx.so)
+        system_ext/lib64/libsource.so)
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
         vendor/lib/hw/audio.primary.mt6785.so | vendor/lib64/hw/audio.primary.mt6785.so)
