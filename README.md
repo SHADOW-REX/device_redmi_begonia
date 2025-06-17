eb65adfec88bc1a97bc66029b407ad40e7be38d4
SHADOW <33841315+SHADOW-REX@users.noreply.github.com>
2025-06-17 13:57:39 +0530

    tree: initial skeleton tree
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Redmi Redmi Note 8 Pro (begonia)
+
+```
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_begonia.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from begonia device
+$(call inherit-product, device/xiaomi/begonia/device.mk)
+
+PRODUCT_DEVICE := begonia
+PRODUCT_NAME := lineage_begonia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 8 Pro
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.1.0.RGGMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.1.0.RGGMIXM:user/release-keys
